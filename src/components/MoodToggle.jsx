9e478a61666927949d7373d43fc0bc0c5ac5482f
DKH <derek.hawryluk@gmail.com>
2025-07-23 09:31:07 -0600

    editing dark mode settings

--- src/components/MoodToggle.jsx
+++ src/components/MoodToggle.jsx
@@ -1,15 +1,22 @@
 const MoodToggle = ({ mood, setMood }) => {
   const handleToggle = () => {
-    setMood(prev => (prev === "dark" ? "light" : "dark"));
+    const newMood = mood === "dark" ? "light" : "dark";
+    setMood(newMood);
+
+    if (newMood === "dark") {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
   };
 
   return (
-    <div className="absolute inset-0 flex items-end justify-center z-50 mb-70">
+    <div className="absolute inset-0 flex items-end justify-center z-50 mb-40">
       <button
         onClick={handleToggle}
-        className="w-fit px-4 py-3 text-xl rounded-full font-bold text-white bg-softFern hover:bg-leafGreen transition duration-300"
+        className="w-fit px-4 py-3 text-xl rounded-full font-bold text-white bg-coastalTeal hover:bg-seafoamMist dark:bg-softFern dark:hover:bg-leafGreen transition duration-300"
       >
-        Mood: {mood === "dark" ? "🌙 Dark" : "🌞 Light"}
+        Mood: {mood === "dark" ? "🍃 Dark" : "🌴 Light"}
       </button>
     </div>
   );
